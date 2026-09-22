da9f15ee4f9125c1dfea760f2c5e7b99c7b89d81
Leander-PC <Leander_TJW@users.noreply.codeberg.org>
2026-09-22 23:56:42 +0200

    Registry has fallback for enums

--- src/Leander.Parsing/ConverterRegistry.cs
+++ src/Leander.Parsing/ConverterRegistry.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Leander.Parsing.Internal;
+
 namespace Leander.Parsing;
 
 public sealed class ConverterRegistry
@@ -9,7 +12,20 @@ public sealed class ConverterRegistry
         _converters = converters;
     }
 
-    public IConverter<T> GetConverter<T>(string? key = null) => (IConverter<T>)_converters[(typeof(T), key)];
+    public IConverter<T> GetConverter<T>(string? key = null)
+    {
+        if (_converters.TryGetValue((typeof(T), key), out var value))
+        {
+            return (IConverter<T>)value;
+        }
+
+        if (key is null && TryCreateEnumConverter<T>() is { } enumConverter)
+        {
+            return enumConverter;
+        }
+
+        return (IConverter<T>)_converters[(typeof(T), key)];
+    }
 
     public bool TryGetConverter<T>(out IConverter<T>? converter, string? key = null)
     {
@@ -19,7 +35,19 @@ public sealed class ConverterRegistry
             return true;
         }
 
-        converter = null;
-        return false;
+        converter = key is null ? TryCreateEnumConverter<T>() : null;
+        return converter is not null;
+    }
+
+    private static IConverter<T>? TryCreateEnumConverter<T>()
+    {
+        if (!typeof(T).IsEnum)
+        {
+            return null;
+        }
+
+        var cacheType = typeof(EnumConverterCache<>).MakeGenericType(typeof(T));
+        var field = cacheType.GetField(nameof(EnumConverterCache<DayOfWeek>.Instance), BindingFlags.Public | BindingFlags.Static)!;
+        return (IConverter<T>)field.GetValue(null)!;
     }
 }
